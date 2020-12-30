59b2a8eae14a30409405c1cdd735cd021c812d6b
Louise <louisepeiffer@hotmail.com>
2020-12-30 11:44:08 +0100

    exo fini

---
+++ public/js/main.js
@@ -0,0 +1,30 @@
+// 1
+let nombre = 8.56
+// 2
+// nombre = Math.round(nombre)
+// console.log(nombre);
+// 3
+// nombre = Math.ceil(nombre)
+// console.log(nombre);
+// 4
+//nombre = Math.floor(nombre)
+// console.log(nombre);
+// 5 
+// nombre = Math.trunc(nombre)
+// console.log(nombre);
+// 6
+//console.log(Math.random());
+// 7
+//console.log(Math.random()*100);
+// 8
+//console.log(Math.round(Math.random()*100));
+// 9
+//console.log(Math.pow(8, 2));
+// 10
+//console.log(Math.sqrt(9));
+// 11
+//console.log(Math.abs(-1));
+// 12
+//console.log(Math.max(-2, 1000, 8, 57));
+// 13
+console.log(Math.min(-2, 1000, 8, 57));
